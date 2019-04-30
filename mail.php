fa9495c02fc8ace83d91fa1badf9087f5432b7a7
Voodeq <deqvoo@gmail.com>
2019-04-30 20:36:05 +0300

    Second Commit

--- mail.php
+++ mail.php
@@ -11,15 +11,15 @@ $message = $_POST['message'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.ukr.net';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'redkit@ukr.net'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'qwe7776671'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('gomudusu@p33.org');     // Кому будет уходить письмо 
+$mail->setFrom('redkit@ukr.net'); // от кого будет уходить письмо?
+$mail->addAddress('newdeq@gmail.com');     // Кому будет уходить письмо
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -29,7 +29,7 @@ $mail->addAddress('gomudusu@p33.org');     // Кому будет уходить
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->Body    = '' .$name . ' залишив повідомлення: ' .$message. '<br> Пошта цього користувача: ' .$email;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
